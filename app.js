12013f84a4a79e58a6d615340640adcdbd3a0e7f
Evgeny_Bobkov <freeedayy@gmail.com>
2021-09-27 21:17:20 +0300

    pacth method refactor

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-    const express = require('express');
-    const mongoose = require('mongoose');
-    const bodyParser = require('body-parser');
-    const cors = require('cors');
-    const app = express();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
 
-    const expensesRouter = require('./src/modules/routes/expenses.router')
+const expensesRouter = require("./src/modules/routes/expenses.router");
 
-    // Подключаемся к Базе данных
-    const uri = "mongodb+srv://Bobkov:Nhk09327rus!@cluster0.yxkny.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-    mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-    //
+// Подключаемся к Базе данных
+const uri =
+	"mongodb+srv://Bobkov:Nhk09327rus!@cluster0.yxkny.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//
 
-    app.use(cors());
-    app.use(bodyParser.json())
-    app.use("/expenses", expensesRouter)
-    app.listen(9000, () => {
-        console.log('Server started');
-    })
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/expenses", expensesRouter);
+
+app.listen(9000, () => {
+	console.log("Server started");
+});
--- src/modules/controllers/expense.controller.js
+++ src/modules/controllers/expense.controller.js
@@ -1,27 +1,44 @@
-const Expense = require('../../db/models/expense/index');
+const Expense = require("../../db/models/expense/index");
 
 module.exports.getAllExpenses = (req, res) => {
-    Expense.find().then(result => {
-        res.send(result);
-    })
+	Expense.find().then((result) => {
+		res.send(result);
+	});
 };
 
 module.exports.createNewExpense = (req, res) => {
-    const expense = new Expense(req.body);
-    expense.save().then(result => res.send('Saved successfully')).catch(e => res.send(e));
+	const expense = new Expense(req.body);
+	expense.save().then((result) => {
+		if (result) {
+			res.send("Saved successfully");
+		} else {
+			res.status(404).json({
+				message: "Что-то пошло не так. Попробуйте снова. (сохранение заметки)",
+			});
+		}
+	});
 };
 
 module.exports.deleteExpense = (req, res) => {
-    const data = req.body;
-    Expense.findByIdAndDelete({_id: data.id})
-        .then(result => res.send('Deleted successfully'))
-        .catch(e => res.send(e));;
+	const id = req.body.id;
+
+	Expense.findByIdAndDelete({ _id: id })
+		.then((result) => res.send("Deleted successfully"))
+		.catch((e) => res.send(e));
 };
 
 module.exports.changeExpense = (req, res) => {
-        const data = req.body;
-        console.log(data);
-        Expense.findOneAndReplace({_id: data.id}, data).then(response => res.send(!!response))
-            .catch(err => res.send(err));
+	const id = req.body.id;
+	const changes = req.body;
 
+	Expense.findOneAndUpdate({ _id: id }, changes).then((expense) => {
+		if (expense) {
+			res.status(200).json(expense);
+		} else {
+			res.status(404).json({
+				message:
+					"Запись не найдена. Возможно, она была удалена. Обновите страницу",
+			});
+		}
+	});
 };
